dff05903833cc3546034c70e0c7613e53e71375f
lahla81 <lahla81@yahoo.com>
2019-05-03 05:29:15 +0200

    first commit

---
+++ precache-manifest.058405560ca8ec3315f087707812adea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7528bca3f7984d3249d7",
+    "url": "/static/js/main.8459fabb.chunk.js"
+  },
+  {
+    "revision": "f7186f2ab020893d8be7",
+    "url": "/static/js/2.2a89fa20.chunk.js"
+  },
+  {
+    "revision": "7528bca3f7984d3249d7",
+    "url": "/static/css/main.4547f961.chunk.css"
+  },
+  {
+    "revision": "f7186f2ab020893d8be7",
+    "url": "/static/css/2.bd2a956d.chunk.css"
+  },
+  {
+    "revision": "114668b9bf991033b21ecf2c51551349",
+    "url": "/index.html"
+  }
+];
